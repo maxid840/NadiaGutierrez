25ff7128cabcc0ffbd7b88237c525da898f15fac
nadiagutierrez89 <nadiagutierrez89@yahoo.com.ar>
2015-04-05 22:45:09 -0300

    TP#0
    TP#0

---
+++ Hola Mundo.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Hola.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+/* TP#0: Mostrar por pantalla "Hola Mundo"
+ * Autor: Nadia E. Gutierrez
+ * Fecha: 05/04/2015 */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hola Mundo!!");
+	return 0;
+}
